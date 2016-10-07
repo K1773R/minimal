2aa2e19821e8915ecb0ac08157b3d1e136f8d31b
K1773R <K1773R@darkgamex.ch>
2016-10-07 11:33:04 +0200

    add yaboot download handling and config

---
+++ src/yaboot_get.sh
@@ -0,0 +1,49 @@
+#!/bin/sh -e
+
+echo "*** GET YABOOT BEGIN ***"
+
+SRC_DIR=$(pwd)
+
+# We default to using prebuilt yaboot dpkg packages because of no active source hosting
+# Also its abandonware, no new versions or anything. launchpad has the latest source/build
+
+# Grab everything after the '=' character.
+DOWNLOAD_URL=$(grep -i ^YABOOT_BINARY_URL .config | cut -f2 -d'=')
+
+# Grab everything after the last '/' character.
+ARCHIVE_FILE=${DOWNLOAD_URL##*/}
+
+# Read the 'USE_LOCAL_SOURCE' property from '.config'
+USE_LOCAL_SOURCE="$(grep -i ^USE_LOCAL_SOURCE .config | cut -f2 -d'=')"
+
+if [ "$USE_LOCAL_SOURCE" = "true" -a ! -f $SRC_DIR/source/$ARCHIVE_FILE  ] ; then
+  echo "Bundle $SRC_DIR/source/$ARCHIVE_FILE is missing and will be downloaded."
+  USE_LOCAL_SOURCE="false"
+fi
+
+cd source
+
+if [ ! "$USE_LOCAL_SOURCE" = "true" ] ; then
+  # Downloading Yaboot bundle file. The '-c' option allows the download to resume.
+  echo "Downloading Yaboot bundle from $DOWNLOAD_URL"
+  wget -c $DOWNLOAD_URL
+else
+  echo "Using local Yaboot bundle $SRC_DIR/source/$ARCHIVE_FILE"
+fi
+
+# Delete folder with previously extracted Yaboot.
+echo "Removing Yaboot work area. This may take a while..."
+rm -rf ../work/yaboot
+mkdir ../work/yaboot
+
+# Extract Yaboot to folder 'work/yaboot'.
+#tar -xvf $ARCHIVE_FILE -C ../work/yaboot
+ar xv $ARCHIVE_FILE data.tar.gz
+# Extract only neccesarry files
+tar xvf data.tar.gz -C ../work/yaboot ./usr/lib/yaboot/yaboot ./usr/share/doc/yaboot/examples/simpleboot.chrp
+rm -f data.tar.gz
+
+cd $SRC_DIR
+
+echo "*** GET YABOOT END ***"
+
